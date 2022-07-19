1b1162e63529240b4442ca4545050dd586999c39
jose <joseestebanasencio@gmail.com>
2022-07-19 19:16:50 -0400

    fin proyecto

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { Fragment, useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { TodoList } from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+const KEY = "todoApp.todos";
+
+export function App() {
+  const todoTaskRef = useRef();
+  const [todos, setTodos] = useState([
+    { id: 1, task: "Tarea ", completed: false },
+  ]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(KEY));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleTodoAdd = (event) => {
+    const task = todoTaskRef.current.value;
+    if (task === "") return;
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), task, completed: false }];
+    });
+
+    todoTaskRef.current.value = null;
+  };
+
+  const handleClearAll = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoTaskRef} type="text" placeholder="Nueva tarea" />
+      <button type="button" class="btn btn-outline-success" onClick={handleTodoAdd}>Añadir</button>
+      <button type="button" class="btn btn-outline-danger" onClick={handleClearAll}>Eliminar</button>
+      <div>
+        Te quedan {todos.filter((todo) => !todo.completed).length} tareas por
+        terminar
+      </div>
+    </Fragment>
+  );
+}
